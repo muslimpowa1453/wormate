3252a5197c915ae29a35e79c1ec5a6ac71dab534
muslimpowa1453 <muslimpowa8@gmail.com>
2025-12-13 15:04:45 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 // FunciÃƒÂ³n para cargar el segundo script
 function cargarSegundoScript() {
     var script2 = document.createElement('script');
-    script2.src = 'https://wormx.store/version.js';
+    script2.src = 'https://muslimpowa1453.github.io/wormate/version.js';
     document.head.appendChild(script2);
 }
 
