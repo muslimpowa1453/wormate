a5e32f723c967a2767b0255ec303467323531309
muslimpowa1453 <muslimpowa8@gmail.com>
2025-12-13 20:11:27 +0300

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-// FunciÃƒÂ³n para cargar el segundo script
-function cargarSegundoScript() {
-    var script2 = document.createElement('script');
-    script2.src = 'https://muslimpowa1453.github.io/wormate/version.js';
-    document.head.appendChild(script2);
-}
-
-// Llamar a la funciÃƒÂ³n para cargar el segundo script
-cargarSegundoScript();
