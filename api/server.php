8cc72a7279fef95bcc537c7e527057d997fb20c7
muslimpowa1453 <muslimpowa8@gmail.com>
2025-12-13 20:06:09 +0300

    Update server.php

--- api/server.php
+++ api/server.php
@@ -3017,7 +3017,7 @@
         },
         {
             "id": 44,
-            "name": "Muslim 👑 01",
+            "name": "Musli 👑 01",
             "region": "peru",
             "serverUrl": "wss:\/\/fra-c.wormate.io:32053\/wormy",
             "status": 1,
